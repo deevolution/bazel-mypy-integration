e17dcc125423022a5c88b5135e6a2fb84085d468
Ben Levine <blevine@belvederetrading.com>
2023-09-27 11:32:05 -0500

    Update

--- mypy/main.py
+++ mypy/main.py
@@ -5,16 +5,7 @@ from mypy.main import main
 import mypy.modulefinder
 from mypy.version import __version__
 
-# According to https://github.com/python/mypy/blob/v0.971/mypy/version.py
-# - Release versions have the form "0.NNN".
-# - Dev versions have the form "0.NNN+dev" (PLUS sign to conform to PEP 440).
-# - For 1.0 we'll switch back to 1.2.3 form.
-def version_tuple(v: str) -> Tuple[int, ...]:
-    """Silly method of creating a comparable version object"""
-    return tuple(map(int, (v.split("+")[0].split("."))))
-
 if __name__ == '__main__':
-    # 0.981 began requiring keyword arguments
     additional_package_paths = [p for p in sys.path if 'pypi__' in p]
     original_get_site_packages_dirs = mypy.modulefinder.get_site_packages_dirs
 
@@ -25,8 +16,4 @@ if __name__ == '__main__':
 
     mypy.modulefinder.get_site_packages_dirs = get_site_packages_dirs
 
-    if version_tuple(__version__) < (0, 981):
-        main(None, sys.stdout, sys.stderr)
-    else:
-        # The first arg was removed
-        main(stdout=sys.stdout, stderr=sys.stderr)
+    main(stdout=sys.stdout, stderr=sys.stderr)
